5b0f98efbdae38f68b19aebc698a93348d6ad3d8
lucas-faget <lucasfaget98@gmail.com>
2026-03-02 20:36:15 +0100

    build: add and configure adonis-autoswagger

---
+++ config/swagger.ts
@@ -0,0 +1,28 @@
+import path from 'node:path'
+import url from 'node:url'
+
+export default {
+    path: path.dirname(url.fileURLToPath(import.meta.url)) + '/../',
+    title: 'Food Api',
+    version: '1.0.0',
+    description: '',
+    tagIndex: 2,
+    info: {
+        title: 'Food Api',
+        version: '1.0.0',
+        description: '',
+    },
+    snakeCase: true,
+    debug: true,
+    ignore: ['/swagger', '/docs'],
+    preferredPutPatch: 'PUT',
+    common: {
+        parameters: {},
+        headers: {},
+    },
+    securitySchemes: {},
+    authMiddlewares: ['auth', 'auth:api'],
+    defaultSecurityScheme: 'BearerAuth',
+    persistAuthorization: true,
+    showFullPath: false,
+}
--- start/routes.ts
+++ start/routes.ts
@@ -8,10 +8,19 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
 
 const FoodController = () => import('#controllers/food_controller')
 const ProductsController = () => import('#controllers/products_controller')
 
-router.get('/food/:barcode', [FoodController, 'show'])
+router.get('/food/:barcode', [FoodController, 'getProduct'])
 router.get('/food', [FoodController, 'search'])
 router.resource('products', ProductsController).apiOnly()
+
+router.get('/swagger', async () => {
+    return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+router.get('/docs', async () => {
+    return AutoSwagger.default.ui('/swagger', swagger)
+})
